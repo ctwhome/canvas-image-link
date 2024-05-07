8aaaedcf55ec345a9740b18416f58304fb6f2fef
Jesús García <ctw@ctwhome.com>
2024-05-07 11:59:28 +0200

    initial commit

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, Canvas, CanvasEdge, CanvasNode, ItemView, } from 'obsidian';
+// import { CanvasEdgeData } from "obsidian/canvas";
 
 interface MyPluginSettings {
 	mySetting: string;
@@ -16,70 +15,97 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		// add duve to tge ribbon
+		this.addRibbonIcon('dices', 'Greet', () => {
+			new Notice('Hello, world!');
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		const canvasView = app.workspace.getActiveViewOfType(ItemView);
+		console.log('🎹 canvasView', canvasView);
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
+		const checking = false;
+		// this.canvas = new CanvasEdgeData(this.app);
+		// Register a click event listener specifically for image clicks in the MarkdownView
+		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
+			const target = evt.target as HTMLElement;
+
+			// Check if the clicked element is an image within a MarkdownView
+			if (target.tagName === 'IMG' && this.app.workspace.getActiveViewOfType(MarkdownView)) {
+				evt.preventDefault();  // Prevent any default behavior
+				this.openImageModal(target as HTMLImageElement);
 			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			// console.log('🎹 this', this);
+
+
+			const canvasView = this.app.workspace.getActiveViewOfType(ItemView);
+			if (canvasView?.getViewType() === "canvas") {
+				// If checking is true, we're simply "checking" if the command can be run.
+				// If checking is false, then we want to actually perform the operation.
+				if (!checking) {
+					// @ts-ignore
+					const canvas = canvasView?.canvas;
+					const currentSelection = canvas?.selection;
+					// console.log('🎹 currentSelection.size', currentSelection.size, currentSelection);
+
+					if (currentSelection.size > 1) {
+						return;
+					}
+
+					if (currentSelection.size === 1) {
+						// check if file path ends with an image extension
+						const node = currentSelection.values().next().value;
+						const filePath = node.filePath;
+						const isImageOrVideo = /\.(png|jpe?g|gif|bmp|svg|webp|ico|tiff?|mp4|mov|avi|wmv|flv|mkv)$/i.test(filePath);
+						console.log('🎹 node image?', !!isImageOrVideo, node.id, node);
+
+						if (isImageOrVideo) {
+							// new ExampleModal().open();
+							const modal = new ExampleModal(this.app);
+							modal.open();
+						}
+
+
+						// const node = canvas.createTextNode({
+						// 	pos: {
+						// 		x: 0,
+						// 		y: 0,
+						// 		height: 500,
+						// 		width: 400
+						// 	},
+						// 	size: {
+						// 		x: 0,
+						// 		y: 0,
+						// 		height: 500,
+						// 		width: 400
+						// 	},
+						// 	text: "",
+						// 	focus: true,
+						// 	save: true,
+						// });
+
+						// canvas.addNode(node);
+						canvas.requestSave();
+						// if (!node) return;
+
+						setTimeout(() => {
+							// node.startEditing();
+							// canvas.zoomToSelection();
+						}, 0);
 					}
 
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
+
+
 			}
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		// Additional plugin initialization...
 	}
 
 	onunload() {
-
+		// Clean up when the plugin is unloaded
 	}
 
 	async loadSettings() {
@@ -89,20 +115,40 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	openImageModal(image: HTMLImageElement) {
+		const modal = new ImageModal(this.app, image.src);
+		modal.open();
+	}
+
+
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class ImageModal extends Modal {
+	imageUrl: string;
+
+	constructor(app: App, imageUrl: string) {
 		super(app);
+		this.imageUrl = imageUrl;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.empty();
+
+		contentEl.createEl('h1', { text: 'Image Details' });
+		contentEl.createEl('img', {
+			attr: {
+				src: this.imageUrl,
+				alt: 'Loaded Image'
+			},
+			cls: 'modal-image-preview'
+		});
+		contentEl.createEl('p', { text: `URL: ${this.imageUrl}` });
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,7 +162,7 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
@@ -130,5 +176,22 @@ class SampleSettingTab extends PluginSettingTab {
 					this.plugin.settings.mySetting = value;
 					await this.plugin.saveSettings();
 				}));
+	}
+}
+
+
+export class ExampleModal extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		let { contentEl } = this;
+		contentEl.setText("Look at me, I'm a modal! 👀");
+	}
+
+	onClose() {
+		let { contentEl } = this;
+		contentEl.empty();
 	}
 }
